fe388dfee05ea49140d4758ed809c79f4eb04391
Marc Shapiro <Marcus@Marcs-MacBook-Air.local>
2015-01-11 20:17:48 -0500

    added show and images control

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -15,26 +15,33 @@
 
     })
 
-    .controller('AboutCtrl', function ($scope) {
+    .controller('AboutCtrl', function ($scope, $http) {
 
       // Slider Control
       $scope.interval = 5000;
       $scope.slides = [];
       var path = '../res/img/';
-      var images = [
-          'ava.jpg',
-          //'ava2.jpg',
-          'eota.jpg',
-          'wedding.jpg',
-          'penny.jpg',
-          'penny-cotton.jpg'
-      ];
 
-      _.each(images, function(image) {
-        $scope.slides.push({
-          image: path + image
-        })
+      $http.get('../images.json')
+          .success(function(data) {
+            console.log('success');
+            _.each(data.images, function(image) {
+              $scope.slides.push({
+                image: path + image
+              });
+            })
       });
+
+      //var images = [
+      //    'ava.jpg',
+      //    //'ava2.jpg',
+      //    'eota.jpg',
+      //    'wedding.jpg',
+      //    'penny.jpg',
+      //    'penny-cotton.jpg'
+      //];
+
+
 
     })
 
@@ -42,9 +49,9 @@
 
     .controller('ShowCtrl', function ($scope) {
       var shows = [
-        ['1.10.15', 'Big Fattys BBQ', '6-8 pm', 'White River Jct., Vt'],
         ['2.05.15', 'The Lyme Inn', '6-8:30 pm', 'Lyme, NH'],
-        ['2.07.15', 'Big Fattys BBQ', '6-8 pm', 'White River Jct., Vt'],
+        ['2.07.15', 'Big Fattys BBQ', '6-8 pm', 'White River Jct., VT', 'http://www.maplestreetcatering.com/piggery.html'],
+        ['2.27.15', 'Big Fattys BBQ', '6-8 pm', 'White River Jct., VT', 'http://www.maplestreetcatering.com/piggery.html'],
         ['3.05.15', 'AVA Gallery', '6-8 pm', 'Lebanon, NH'],
         ['3.13.15', 'Riverwalk Cafe', '8 pm', 'Nashua, NH'],
         ['7.24.15', 'Private Event', '', 'Sunapee, NH']
@@ -56,10 +63,12 @@
           date: show[0],
           venue: show[1],
           time: show[2],
-          location: show[3]
+          location: show[3],
+          venueSite: show[4]
         })
       })
     })
+
 
     .controller('SoundCtrl', function ($scope) {
 
